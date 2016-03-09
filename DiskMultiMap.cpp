ad8a984d0e6c8088b76b9d51a3379f861d99c8ed
berylnm <wby14218@gmail.com>
2016-03-09 11:32:16 -0800

    Added files via upload
    maybe finished? no concrete test though

--- DiskMultiMap.cpp
+++ DiskMultiMap.cpp
@@ -13,55 +13,296 @@
 using namespace std;
 
 DiskMultiMap::DiskMultiMap(){}
-DiskMultiMap::~DiskMultiMap(){}
+DiskMultiMap::~DiskMultiMap()
+{
+    if (m_file.isOpen())
+        close();
+}
 
 bool DiskMultiMap::createNew(const std::string& filename, unsigned int numBuckets)
 {
-    if (b.isOpen())
-        b.close();
+    if (m_file.isOpen())
+        m_file.close();
     
-    b.createNew(m_free);
-    b.write(freememory(0,0,0),0);   //head structrue for freed space 0 used, 0 in total
-    b.close();
+    m_file.createNew(m_free);
+    m_file.write(head(0,0,0),0);   //head structrue for freed space 0 used, 0 in total
+    m_file.close();
     
-    if (!b.createNew(filename))
+    if (!m_file.createNew(filename))
         return false;
     m_fn = filename;
     m_bucket = numBuckets;
-    b.write(freememory(0,0,0),0);   //head structure for DiskMultiMap 0 used, 0 in total
+    m_file.write(head(0,0,0),0);   //head structure for DiskMultiMap  0 used, 0 in total
     for (int i=0; i<numBuckets; i++)
     {
-        string a;
-        Bucket bk(a);
+        Bucket bk;
         bk.next = 0;
-        bk.nextbucket = 0;
         bk.used = false;
-        b.write(bk,b.fileLength());
+        m_file.write(bk,m_file.fileLength());
         
     }
+    int n = m_file.fileLength();
+    m_file.close();
     return true;
 }
 
 bool DiskMultiMap::openExisting(const std::string &filename)
 {
-    if (!b.openExisting(filename))
+    if (!m_file.openExisting(filename))
         return false;
     return true;
 }
 
 void DiskMultiMap::close()
 {
-    if (b.isOpen())
-        b.close();
+    if (m_file.isOpen())
+        m_file.close();
 }
 bool DiskMultiMap::insert(const std::string& key, const std::string& value, const std::string& context)
 {
     if (key.size() > 120 || value.size() > 120 || context.size() > 120)
         return false;
+    if (!m_file.openExisting(m_fn))
+        return false;
+    BinaryFile::Offset prev = 0;
+    BinaryFile::Offset Offset = keyhash(key);
+    BinaryFile::Offset start = 0;
+    int k = m_file.fileLength();
+    
+    m_file.close();
     
     
+    m_file.openExisting(m_free);
+    head h = head(0,0,0);
+    m_file.read(h,0);
+    if (h.numinuse != 0)
+    {   if (h.next==0)
+            cout<<"Error, wrong link";
+        start = h.next;
+        m_file.write(head(h.numinuse-1,h.numintotal,h.next),0);
+        freememory t = freememory(0,0);
+        m_file.read(t,h.next);
+        m_file.write(freememory(0,t.next),start);
+       
+    }
+    if (start == 0 )
+        start = k;
+    m_file.close();
+    
+    
+    m_file.openExisting(m_fn);
+    Bucket bk;
+    m_file.read(bk,Offset);
+    if (bk.used == false)
+    {
+        strcpy(bk.key,key.c_str());
+        bk.next = start;
+        bk.used = true;
+        m_file.write(bk,Offset);
+        m_file.write(DiskNode(key,value,context,0),start);
+    }
+    else
+    {   prev = bk.next;
+        bk.next = start;
+        m_file.write(bk,Offset);
+        m_file.write(DiskNode(key,value,context,prev),start);
+    }
+    
+    
+//for test purpose///////////////
+    DiskNode d;
+    m_file.read(d,start);
+    m_file.read(bk,Offset);
+//need to comment out it/////////////
+
+    m_file.close();
+    return true;
 }
-BinaryFile::Offset DiskMultiMap::hasher(string key)
+
+BinaryFile::Offset DiskMultiMap::keyhash(string key)
 {
+    hash<string> hasher;
+    unsigned int rkey = hasher(key);
+    Bucket bk(key);
+    head a = head(0,0,0);
+    rkey = (((rkey % (m_bucket))*sizeof(bk))+sizeof(a));
+    return rkey;
+}
+int DiskMultiMap::erase(const std::string& key, const std::string& value, const std::string& context)
+{
+    if (!m_file.openExisting(m_fn))
+        return -1;
+    int num = 0;
+    BinaryFile::Offset Offset = keyhash(key);
+    Bucket b;
+    if (m_file.read(b,Offset) && b.used == false)
+    {   m_file.close();
+        return 0;
+    }
+    
+    DiskNode current;
+    m_file.read(current,b.next);
+    BinaryFile::Offset os = b.next;
+    BinaryFile::Offset prev = Offset;
+    for(;;)
+    {
+        if (strcmp(current.key,key.c_str())!=0 || strcmp(current.value,value.c_str())!=0 || strcmp(current.context,context.c_str())!=0)
+        {
+            if (current.next == 0)
+            {   m_file.close();
+                return num;
+            }
+            prev = os;
+            os = current.next;
+            m_file.read(current,current.next);
+            continue;
+        }
+        
+        else
+        {   num++;
+            m_file.close();
+            if (!m_file.openExisting(m_free))
+            {   m_file.close();
+                return -1;
+            }
+            head h = head(0,0,0);
+            BinaryFile::Offset pos = 0;
+            if (m_file.read(h,0)&&h.numinuse!=h.numintotal)
+            {   pos = h.next;
+                freememory f = freememory(0,0);
+                m_file.read(f,f.next);
+                while(f.num ==0)
+                {
+                    pos = f.next;
+                    m_file.read(f,f.next);
+                }
+                m_file.read(f,pos);
+                m_file.write(freememory(os,f.next),pos);
+                m_file.write(head(h.numinuse+1,h.numintotal,h.next),0);
+                m_file.close();
+                
+            }
+            else
+            {
+                m_file.write(freememory(os,0),m_file.fileLength());
+                m_file.write(head(h.numinuse+1,h.numintotal+1,h.next),0);
+                m_file.close();
+            }
+        }
+        m_file.openExisting(m_fn);
+        DiskNode pv;
+        m_file.read(pv,prev);
+        m_file.write(DiskNode(pv.key,pv.value,pv.context,current.next),prev);
+        if (current.next == 0)
+            break;
+        else
+            m_file.read(current,current.next);
+        }
+    
+    m_file.close();
+    return num;
+}
+DiskMultiMap::Iterator DiskMultiMap::search(const std::string &key)
+{
+    if (!m_file.openExisting(m_fn))
+        cerr<<"Error, no such file"<<endl;
+    BinaryFile::Offset offset = keyhash(key);
+    Bucket b;
+    m_file.read(b,offset);
+    offset = b.next;
+    if (b.used == false || b.next == 0)
+    {
+        Iterator it;
+        m_file.close();
+        return it;
+    }
+    else
+    {   DiskNode d;
+        m_file.read(d,offset);
+        while(key!=d.key && d.next!=0)
+        {
+            offset = d.next;
+            m_file.read(d,d.next);
+        }
+    }
+        Iterator it = Iterator(this,offset);
+    m_file.close();
+    return it;
     
 }
+///////////////////////////////////////////////
+////////ITERATOR//////////////////////////////
+////////IMPLEMENTATION////////////////////////
+//////////////////////////////////////////////
+DiskMultiMap::Iterator::Iterator()
+{
+    m_valid = false;
+    p = nullptr;
+}
+DiskMultiMap::Iterator::Iterator(DiskMultiMap* d, BinaryFile::Offset o)
+{
+    m_valid = true;
+    p = d;
+    offset = o;
+}
+bool DiskMultiMap::Iterator::isValid() const
+{
+    return m_valid;
+}
+MultiMapTuple DiskMultiMap::Iterator::operator*()
+{
+    MultiMapTuple m;
+    if (!this->m_valid)
+        return m;
+    else
+    {
+       if (!p->openExisting(p->m_fn))
+       {    p->close();
+           return m;
+       }
+        else
+        {
+            DiskNode d;
+            p->m_file.read(d,this->offset);
+            m.key = d.key;
+            m.value= d.value;
+            m.context = d.context;
+            
+        }
+    }
+    p->close();
+    return m;
+    
+}
+DiskMultiMap::Iterator& DiskMultiMap::Iterator::operator++()
+{
+    DiskNode d;
+    if (!p->openExisting(p->m_fn))
+        cerr<<"didnt open";
+    p->m_file.read(d,this->offset);
+    if (d.next == 0)
+        m_valid = false;
+    else
+        offset = d.next;
+    p->close();
+    return *this;
+        
+}
+void DiskMultiMap::printlist(string key)
+{
+    if (!openExisting(m_fn))
+        return;
+    BinaryFile::Offset offset = keyhash(key);
+    Bucket b;
+    m_file.read(b,offset);
+    DiskNode d;
+    m_file.read(d,b.next);
+    while (d.next!=0)
+    {
+        cout<<d.key<<" "<<d.value<<" "<<d.context<<endl;
+        m_file.read(d,d.next);
+    }
+    cout<<d.key<<" "<<endl;
+    m_file.close();
+}
+
